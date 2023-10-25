3cafd50c33c54432d69d20d7e09487246ce96a14
Carl Marc <148695036+carlmarc01@users.noreply.github.com>
2023-10-25 11:25:43 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        String x = "Hello! " ;
+        String y = "World ";
+        System.out.println("Let's say " + x + "to the " + y + "of Java Programming!");
+    }
+}
